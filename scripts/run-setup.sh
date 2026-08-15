7aea7c1eeb8f306a57775cbbcd18cf8f28961dbc
TheOnly-Coder <148651205+TheOnly-Coder@users.noreply.github.com>
2026-08-16 00:49:43 +0100

    Delete scripts directory

--- scripts/run-setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=$HOME/.forge-build/jdk8
-export PATH=$JAVA_HOME/bin:$PATH
-cd /home/z/my-project/heat-client
-$HOME/.forge-build/gradle-2.14/bin/gradle setupDecompWorkspace --no-daemon --console=plain > /home/z/my-project/heat-client/setup.log 2>&1
-echo "EXIT_CODE=$?" >> /home/z/my-project/heat-client/setup.log
